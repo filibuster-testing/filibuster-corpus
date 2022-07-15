c20c92265455fba7a9467ff2df147e678fcf296e
Lydia Stark <lydiarstark@gmail.com>
2022-07-15 10:43:12 -0400

    added case study 2 example c. Auth service now has 3 methods that use conditional logic to determine whether they should grant authorization for a delivery or takeout order

---
+++ case-study-2-food-delivery-example-c/functional/automated_tests.py
@@ -0,0 +1,152 @@
+#!/usr/bin/env python
+
+import requests
+import os
+import sys
+import json
+
+examples_path = os.path.dirname(
+    os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+)
+sys.path.append(examples_path)
+
+import helper
+
+helper = helper.Helper("case-study-2-food-delivery-example-c")
+
+
+def create_delivery_order():
+    order_amount = 11.89
+    response = requests.post(
+        "{}/orders/delivery".format(helper.get_service_url("orders")),
+        timeout=helper.get_timeout("orders"),
+        json={"order_amount": order_amount},
+    )
+    return response
+
+
+def delete_delivery_order():
+    order_amount = 15.89
+    response = requests.delete(
+        "{}/orders/delivery/10".format(helper.get_service_url("orders")),
+        timeout=helper.get_timeout("orders"),
+        json={"order_id": 10, "order_amount": order_amount},
+    )
+    return response
+
+
+def update_delivery_order():
+    order_amount = 15.89
+    response = requests.put(
+        "{}/orders/delivery/10".format(helper.get_service_url("orders")),
+        timeout=helper.get_timeout("orders"),
+        json={"order_id": 10, "order_amount": order_amount},
+    )
+    return response
+
+
+def create_takeout_order():
+    order_amount = 13.78
+    response = requests.post(
+        "{}/orders/takeout".format(helper.get_service_url("orders")),
+        timeout=helper.get_timeout("orders"),
+        json={"order_amount": order_amount},
+    )
+    return response
+
+
+def delete_takeout_order():
+    order_amount = 13.78
+    response = requests.delete(
+        "{}/orders/takeout/11".format(helper.get_service_url("orders")),
+        timeout=helper.get_timeout("orders"),
+        json={"order_id": 11, "order_amount": order_amount},
+    )
+    return response
+
+
+def update_takeout_order():
+    order_amount = 18.89
+    response = requests.put(
+        "{}/orders/takeout/11".format(helper.get_service_url("orders")),
+        timeout=helper.get_timeout("orders"),
+        json={"order_id": 11, "order_amount": order_amount},
+    )
+    return response
+
+
+if __name__ == "__main__":
+
+    # If 'DELETE_FAULT' environment variable is set, authorization is refused for deleting all takeout orders.
+    # Note that 'DELETE_FAULT must be set for both the Takeout Auth service and this test file.
+    if os.environ.get("DELETE_FAULT"):
+
+        # these assertions will be true, becuase authorization is being granted for everything except deleting takout orders
+        create_delivery_response = create_delivery_order()
+        assert create_delivery_response.status_code == 201
+
+        update_delivery_response = update_delivery_order()
+        assert update_delivery_response.status_code == 200
+
+        delete_delivery_response = delete_delivery_order()
+        assert delete_delivery_response.status_code == 200
+
+        create_takeout_response = create_takeout_order()
+        assert create_takeout_response.status_code == 201
+
+        update_takeout_response = update_takeout_order()
+        assert update_takeout_response.status_code == 200
+
+        # five repeated failures to authorize deleting a takeout order will trip the corresponding circuit breaker.
+        # As a result of this, on the six attempt to delete a takeout order, we will see the status code change from 500 to 503,
+        # since the circuit breaker is now open and rendering the Takeout Auth service unavailable.
+        for i in range(6):
+            delete_takeout_response = delete_takeout_order()
+            try:
+                assert delete_takeout_response.status_code == 200
+            except AssertionError as e:
+                print(
+                    "Assertion error for delete authorization: status code",
+                    delete_takeout_response.status_code,
+                )
+
+        # Authorization for creating and updating orders still functions properly, since those operations
+        # have seperate invocation paths from deleting orders.
+        # create_delivery_response = create_delivery_order()
+        # assert create_delivery_response.status_code == 201
+
+        update_delivery_response = update_delivery_order()
+        assert update_delivery_response.status_code == 200
+
+        create_takeout_response = create_takeout_order()
+        assert create_takeout_response.status_code == 201
+
+        update_takeout_response = update_takeout_order()
+        assert update_takeout_response.status_code == 200
+
+        # Becuase delivery and takeout orders use the same invocation path for deleting methods (via the 'rpc_auth_delete' method)
+        # the open circuit breaker now prevents authorization for both kinds of orders, so this assertion will fail
+        delete_delivery_response = delete_delivery_order()
+        assert delete_delivery_response.status_code == 200
+
+    # If 'DELETE_FAULT' is not set, everything functions as intended, and authorization is granted for all order operations
+    else:
+
+        # these assertions will all be true
+        create_delivery_response = create_delivery_order()
+        assert create_delivery_response.status_code == 201
+
+        update_delivery_response = update_delivery_order()
+        assert update_delivery_response.status_code == 200
+
+        delete_delivery_response = delete_delivery_order()
+        assert delete_delivery_response.status_code == 200
+
+        create_takeout_response = create_takeout_order()
+        assert create_takeout_response.status_code == 201
+
+        update_takeout_response = update_takeout_order()
+        assert update_takeout_response.status_code == 200
+
+        delete_takeout_response = delete_takeout_order()
+        assert delete_takeout_response.status_code == 200
---
+++ case-study-2-food-delivery-example-c/services/auth/auth/app.py
@@ -0,0 +1,81 @@
+from flask import Flask, jsonify, request, json
+from werkzeug.exceptions import ServiceUnavailable
+import os
+import sys
+
+examples_path = os.path.dirname(
+    os.path.dirname(
+        os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+    )
+)
+sys.path.append(examples_path)
+
+import helper
+
+helper = helper.Helper("case-study-2-food-delivery-example-c")
+
+app = Flask(__name__)
+
+## Instrument using filibuster
+
+sys.path.append(os.path.dirname(examples_path))
+
+from filibuster.instrumentation.requests import (
+    RequestsInstrumentor as FilibusterRequestsInstrumentor,
+)
+
+FilibusterRequestsInstrumentor().instrument(
+    service_name="auth", filibuster_url=helper.get_service_url("filibuster")
+)
+
+from filibuster.instrumentation.flask import (
+    FlaskInstrumentor as FilibusterFlaskInstrumentor,
+)
+
+FilibusterFlaskInstrumentor().instrument_app(
+    app, service_name="auth", filibuster_url=helper.get_service_url("filibuster")
+)
+
+# filibuster requires a health check app to ensure service is running
+@app.route("/health-check", methods=["GET"])
+def auth_health_check():
+    return jsonify({"status": "OK"})
+
+
+# This method grants authorization to delete takeout and delivers orders
+# If the 'DELETE_FAULTS' enivornment variable is set at runtime, it will
+# refuse authorization for deleting takeout orders.
+@app.route("/auth/<order_type>", methods=["DELETE"])
+def auth_delete_takeout(order_type):
+
+    if str(order_type) == "delivery":
+        return jsonify(request.json), 200
+    elif order_type == "takeout":
+        if os.environ.get("DELETE_FAULT", ""):
+            return jsonify(request.json), 500
+        else:
+            return jsonify(request.json), 200
+    else:
+        return jsonify(request.json), 403
+
+
+# This method grants authorization to update a takeout/delivery order
+@app.route("/auth/<order_type>", methods=["PUT"])
+def auth_put_takeout(order_type):
+    if str(order_type) == "delivery" or str(order_type) == "takeout":
+        return jsonify(request.json), 200
+    else:
+        return jsonify(request.json), 403
+
+
+# This method grants authorization to create a takeout/delivery order
+@app.route("/auth/<order_type>", methods=["POST"])
+def auth_post_takeout(order_type):
+    if str(order_type) == "delivery" or str(order_type) == "takeout":
+        return jsonify(request.json), 201
+    else:
+        return jsonify(request.json), 403
+
+
+if __name__ == "__main__":
+    app.run(port=helper.get_port("auth"), host="0.0.0.0", debug=helper.get_debug())

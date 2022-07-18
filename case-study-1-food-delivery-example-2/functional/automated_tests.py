9c7a76b8ab5f4198629904f917122c26b51ee5b6
Lydia Stark <lydiarstark@gmail.com>
2022-07-18 14:10:19 -0400

    Refactored examples a and b in case study 1 to remove order_id from Flask routes

---
+++ case-study-1-food-delivery-example-2/functional/automated_tests.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+
+import requests
+import os
+import sys
+import json
+
+examples_path = os.path.dirname(
+    os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+)
+sys.path.append(examples_path)
+
+import helper
+
+helper = helper.Helper("case-study-1-food-delivery-example-1")
+
+
+def test_create_orders():
+    response = requests.post(
+        "{}/orders".format(helper.get_service_url("orders")),
+        timeout=helper.get_timeout("orders"),
+        json={"order_amount": 11.89},
+    )
+    return response
+
+
+def test_delete_orders():
+    response = requests.delete(
+        "{}/orders".format(helper.get_service_url("orders")),
+        timeout=helper.get_timeout("orders"),
+        json={"order_id": 10, "order_amount": 15.89},
+    )
+    return response
+
+
+def test_update_orders():
+    response = requests.put(
+        "{}/orders".format(helper.get_service_url("orders")),
+        timeout=helper.get_timeout("orders"),
+        json={"order_id": 10, "order_amount": 15.89},
+    )
+    return response
+
+
+if __name__ == "__main__":
+
+    # If 'DELETE_FAULT' is set, authorization is refused for all delete orders
+    # Note that 'DELETE_FAULT must be set for both the auth service and this test file
+    if os.environ.get("DELETE_FAULT"):
+
+        # these assertions will be true, becuase the auth service is still functioning for create and update
+        create_response = test_create_orders()
+        assert create_response.status_code == 201
+
+        update_response = test_update_orders()
+        assert update_response.status_code == 200
+
+        # the repeated failure of the delete authorization will trip the corresponding circuit breaker
+        for i in range(6):
+            delete_response = test_delete_orders()
+            try:
+                assert delete_response.status_code == 200
+            except AssertionError as e:
+                print(
+                    "Assertion error for delete authorization: status code",
+                    delete_response.status_code,
+                )
+
+        # however, since the cirucit breakers for the Orders service have the proper granulairty,
+        # authorization will still be granted for create and update
+        response = test_create_orders()
+        assert response.status_code == 201
+
+        response = test_update_orders()
+        assert response.status_code == 200
+
+    # If 'DELETE_FAULT' is not set, authorization works for all create, update, and delete
+    else:
+
+        # these assertions will be true, becuase the auth service is functioning properly
+        create_response = test_create_orders()
+        assert create_response.status_code == 201
+
+        update_response = test_update_orders()
+        assert update_response.status_code == 200
+
+        delete_response = test_delete_orders()
+        assert delete_response.status_code == 200
